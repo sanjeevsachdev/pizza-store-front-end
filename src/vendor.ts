10d0c32077e9e054496d9d62ddc45e4abdc66db3
Sanjeev Sachdev <sanjeevsachdev@gmail.com>
2017-05-30 22:59:58 +0530

    Brought in webpack.

---
+++ src/vendor.ts
@@ -0,0 +1,25 @@
+// Angular
+import '@angular/animations';
+import '@angular/common';
+import '@angular/compiler';
+import '@angular/core';
+import '@angular/forms';
+import '@angular/http';
+import '@angular/material';
+import '@angular/platform-browser';
+import '@angular/platform-browser-dynamic';
+import '@angular/router';
+
+import 'angular-in-memory-web-api';
+import 'core-js';
+
+import 'ng2-filter-pipe';
+
+// RxJS
+import 'rxjs';
+
+// Angular Material theme
+import '@angular/material/prebuilt-themes/indigo-pink.css';
+
+// Other vendors for example jQuery, Lodash or Bootstrap
+// You can import js, ts, css, sass, ...
